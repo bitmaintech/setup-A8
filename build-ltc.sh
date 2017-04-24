2b43513be4a4d0f823431f3553af77efbde1e412
yang.bai <yang.bai@bitmaintech.com>
2017-04-24 16:04:34 +0800

    Use relative path.

--- build-ltc.sh
+++ build-ltc.sh
@@ -3,24 +3,24 @@ set -x
 type=$1
 freq=$2
 date=`date`
-cg_path="/home/fazio/Desktop/A8/setup-A8/sources/meta-antminer/recipes-bitmianer/cgminer/cgminer-1.0"
-config_path="/home/fazio/Desktop/A8/setup-A8/sources/meta-antminer/recipes-bitmianer/initscripts/initscripts-1.0"
+cg_path="./sources/meta-antminer/recipes-bitmianer/cgminer/cgminer-1.0"
+config_path="./sources/meta-antminer/recipes-bitmianer/initscripts/initscripts-1.0"
 
 ps -aux | grep bitbake |awk {'print $2'} | xargs kill -9
 
-sed -i -r "s/.*?echo \".*?\" > .*?compile_time/        echo \"$date\" > \${D}\${bindir}\/compile_time/g" /home/fazio/Desktop/A8/setup-A8/sources/meta-antminer/recipes-bitmianer/initscripts/initscripts_1.0.bbappend
-sed -i -r "s/.*?echo \".*?\" >> .*?compile_time/        echo \"Antminer $type\" >> \${D}\${bindir}\/compile_time/g" /home/fazio/Desktop/A8/setup-A8/sources/meta-antminer/recipes-bitmianer/initscripts/initscripts_1.0.bbappend
+sed -i -r "s/.*?echo \".*?\" > .*?compile_time/        echo \"$date\" > \${D}\${bindir}\/compile_time/g" ./sources/meta-antminer/recipes-bitmianer/initscripts/initscripts_1.0.bbappend
+sed -i -r "s/.*?echo \".*?\" >> .*?compile_time/        echo \"Antminer $type\" >> \${D}\${bindir}\/compile_time/g" ./sources/meta-antminer/recipes-bitmianer/initscripts/initscripts_1.0.bbappend
 sed -i -r "s/\"bitmain-freq\" : \".*\"/\"bitmain-freq\" : \"$freq\"/g" $config_path/cgminer_l3.conf.factory
 
 rm $cg_path/cgminer-ltc.tar.bz2
 if [ x${type} = "xL3" ];then
 	cp $cg_path/cgminer-ltc.tar.bz2.l3 $cg_path/cgminer-ltc.tar.bz2
-	sed -i -r "s/^Miner_TYPE = \".*\"/\Miner_TYPE = \"L3\"/g" /home/fazio/Desktop/A8/setup-A8/conf/local.conf
+	sed -i -r "s/^Miner_TYPE = \".*\"/\Miner_TYPE = \"L3\"/g" ./conf/local.conf
 fi
 
 if [ x${type} = "xL3+" ];then
 	cp $cg_path/cgminer-ltc.tar.bz2.l3+ $cg_path/cgminer-ltc.tar.bz2
-	sed -i -r "s/^Miner_TYPE = \".*\"/\Miner_TYPE = \"L3+\"/g" /home/fazio/Desktop/A8/setup-A8/conf/local.conf
+	sed -i -r "s/^Miner_TYPE = \".*\"/\Miner_TYPE = \"L3+\"/g" ./conf/local.conf
 fi
 
 
@@ -42,4 +42,4 @@ rm -rf *.tar.gz
 c_time=`date "+%Y%m%d%H%M"`
 file_name="Antminer-${type}-${c_time}-${freq}M.tar.gz"
 tar -zcvf "$file_name" *
-cp $file_name ~/Desktop/l3_firmware
+
